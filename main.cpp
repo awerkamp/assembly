f21ecbb52d08d4a9772b0cdb038084e71329231e
Benjamin Awerkamp <awerkamp@gmail.com>
2021-02-08 15:01:16 -0700

    implement c args to assembly

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
+#include <sys/time.h>
 using namespace std;
 
 extern "C" {
-    void sayHello();    
+    void myPuts();
+    void sayHello();   
+    timeval getTimeOfDay(); 
 }
-
 
 int main(int argc, char **argv) {
 
+    myPuts();
+    std::cout <<  "" << endl; 
     sayHello();
+ 
+    timeval times = getTimeOfDay();
+    std::cout << times.tv_sec << endl;
+    std::cout << times.tv_usec << endl;
+
+    timeval tv;
+
+    gettimeofday(&tv, NULL);
+    std::cout << tv.tv_sec << "\n";
+    std::cout << tv.tv_usec << "\n";
 
     return 0;
-
 }
+
+// 
--- myAsm.s
+++ myAsm.s
@@ -1,20 +1,42 @@
-section .rodata  ; this is the read only data (hello is a constant) 
-helloString: db "hello",0 ; hellostring is the name of our symbol
-; db is the directive to put data in our object file
-; the 0 is the null terminator that puts is expecting.  
-; nasm does NOT null terminate our string automatically
-
+extern _puts
 
 section .text  ; this says that we're about to write code (as opposed to data)
 
-global _sayHello ; this says that "sayHello" is a symbol that the linker needs to know about
-
-_sayHello:           ;and here we go...
-
-  push rbp ; push rbp onto the stack, making it 8 byte aligned
-  pop rbp  ; pop it off the stack so that 
-           ;the caller's return address is now on top of the stack
+  global _sayHello ; this says that "sayHello" is a symbol that the linker needs to know about
+  global _myPuts
+  global _getTimeOfDay
   
-  ;;code goes here
+_getTimeOfDay:
 
-  ret                 ; just return
+    sub rsp, 16
+    mov rax, 0x2000074 
+    mov rdi, rsp
+    mov rsi, 0
+    syscall
+    mov rax, [rsp]
+    mov rdx, [rsp + 8]
+    add rsp, 16
+    ret
+
+_myPuts:           
+
+    mov rax, 0x02000004 ; to write
+    mov rdi, 1 ; 1 is for sdt out
+    mov rsi, myPuts ; source (register source index)
+    mov rdx, 6
+    syscall ; jumps from user mode to kernel mode; checks each register
+    ret              
+
+_sayHello:        
+    
+    push rbp ; base pointer push rbp onto the stack, making it 8 byte aligned
+    mov rdi, helloString
+    call _puts ; for printing
+    pop rbp  ; pop it off the stack so that 
+              ; the caller's return address is now on top of the stack
+    ret                 ; just return
+
+section .data  ; this is the read only data (hello is a constant) 
+  
+  helloString: db "hello",0 ; hellostring is the name of our symbol
+  myPuts: db "myPuts",0
